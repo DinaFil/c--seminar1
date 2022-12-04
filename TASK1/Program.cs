f6ebb1b93dd1c254c8e37d6a97b6781e9cadbb29
Dina Filippova <dinafilippova@gmail.com>
2022-12-04 11:00:19 +0700

    Проба

--- TASK1/Program.cs
+++ TASK1/Program.cs
@@ -1,5 +1,12 @@
-﻿Console.Write("Пожалуйста, введите любое целое число ");
-int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Result: " + number*number);
+﻿Console.WriteLine("Пожалуйста, введите число1 ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Пожалуйста, введите число2 ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+string Answer = 0;
+if (number1*number1)=number2 Answer = "Да"; 
+else Answer = "Нет";
+
+Console.WriteLine(Answer);
+
 
 
