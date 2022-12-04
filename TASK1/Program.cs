5812b6d27ad84724b5c734306d3f92e90bce1124
Dina Filippova <dinafilippova@gmail.com>
2022-12-04 11:09:16 +0700

    square number

--- TASK1/Program.cs
+++ TASK1/Program.cs
@@ -2,11 +2,29 @@
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Пожалуйста, введите число2 ");
 int number2 = Convert.ToInt32(Console.ReadLine());
-string Answer = 0;
-if (number1*number1)=number2 Answer = "Да"; 
-else Answer = "Нет";
+if(number1*number1 == number2)
+{
+Console.WriteLine("Да!"); 
+} 
+else 
+{
+    Console.WriteLine("Нет!");
+}
 
-Console.WriteLine(Answer);
 
+
+
+
+// /*Console.Write("Введите имя пользователя ");
+// string username = Console.ReadLine();
+// if(username.ToLower() == "дина")
+// {
+//     Console.WriteLine("Ура, это же Дина!!");
+// }
+// else
+// {
+// Console.Write("Привет, ");
+// Console.Write(username);
+// }
 
 
